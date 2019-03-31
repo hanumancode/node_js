34dac1fd2c0f46e62d40458958ae00ca990163af
Gareth Jones <hanumancode@gmail.com>
2019-03-31 15:05:25 +0100

    os arch, cpus, memory, uptime...

--- os_demo.js
+++ os_demo.js
@@ -2,3 +2,21 @@ const os = require('os');
 
 // Platform 
 console.log(os.platform());
+
+// CPU Architechure
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+// Total memory
+console.log(os.totalmem());
+
+// Home directory
+console.log(os.homedir());
+
+// Uptime
+console.log(os.uptime());
