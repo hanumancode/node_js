a9dd05503408389821be3c6656b647b13b95231d
Gareth Jones <hanumancode@gmail.com>
2019-03-31 16:21:28 +0100

    url demo added: host, pathname, serialized query...

--- url_demo.js
+++ url_demo.js
@@ -1,6 +1,26 @@
 const url = require('url');
 
-const myUrl = new URL('https://gljmedia.com/test.html?id=100&status=active');
+const myUrl = new URL('https://gljmedia.com:8000/test.html?id=100&status=active');
 
 // Serialized URL
 console.log(myUrl.href);
+console.log(myUrl.toString());
+
+// host (root domain)
+console.log(myUrl.host); // doesn't include the port
+console.log(myUrl.hostname);
+
+// Pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Params - returns object from the serialized query
+console.log(myUrl.searchParams);
+
+// Add param
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+//console.log(myUrl.href);
+
